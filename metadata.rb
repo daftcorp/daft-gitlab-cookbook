75432ff222fa971989af8ce5876a592a81c2bac4
Jeff Tougas <tougasj@gmail.com>
2014-06-16 19:53:15 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'daft-gitlab-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures daft-gitlab-cookbook'
+long_description 'Installs/Configures daft-gitlab-cookbook'
+version          '0.1.0'
+
